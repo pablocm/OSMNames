1f44cdfe6ba96008af9491970cdb8fb3a5638d28
Pablo Cruz <pcruzm@gmail.com>
2019-05-09 17:00:55 -0400

    Add custom postgres config

--- data/postgres_config.sh
+++ data/postgres_config.sh
@@ -10,6 +10,29 @@ function alter_system() {
     -- add your postgres configuration here
     -- recommended: https://pgtune.leopard.in.ua/
     -- with alter system option
+
+    # DB Version: 11
+    # OS Type: linux
+    # DB Type: mixed
+    # Total Memory (RAM): 12 GB
+    # CPUs num: 2
+    # Data Storage: hdd
+
+    ALTER SYSTEM SET max_connections = '100';
+    ALTER SYSTEM SET shared_buffers = '3GB';
+    ALTER SYSTEM SET effective_cache_size = '9GB';
+    ALTER SYSTEM SET maintenance_work_mem = '768MB';
+    ALTER SYSTEM SET checkpoint_completion_target = '0.9';
+    ALTER SYSTEM SET wal_buffers = '16MB';
+    ALTER SYSTEM SET default_statistics_target = '100';
+    ALTER SYSTEM SET random_page_cost = '4';
+    ALTER SYSTEM SET effective_io_concurrency = '2';
+    ALTER SYSTEM SET work_mem = '15728kB';
+    ALTER SYSTEM SET min_wal_size = '1GB';
+    ALTER SYSTEM SET max_wal_size = '2GB';
+    ALTER SYSTEM SET max_worker_processes = '2';
+    ALTER SYSTEM SET max_parallel_workers_per_gather = '1';
+    ALTER SYSTEM SET max_parallel_workers = '2';
 EOSQL
 }
 
