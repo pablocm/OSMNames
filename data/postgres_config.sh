f58d5a1e85391e182972d5d53dc455a35dac0347
Pablo Cruz <pcruzm@gmail.com>
2019-05-15 19:00:25 -0400

    Add custom postgres config

--- data/postgres_config.sh
+++ data/postgres_config.sh
@@ -13,26 +13,26 @@ function alter_system() {
 
     # DB Version: 11
     # OS Type: linux
-    # DB Type: mixed
-    # Total Memory (RAM): 12 GB
-    # CPUs num: 2
-    # Data Storage: hdd
+    # DB Type: dw
+    # Total Memory (RAM): 64 GB
+    # CPUs num: 4
+    # Data Storage: ssd
 
-    ALTER SYSTEM SET max_connections = '100';
-    ALTER SYSTEM SET shared_buffers = '3GB';
-    ALTER SYSTEM SET effective_cache_size = '9GB';
-    ALTER SYSTEM SET maintenance_work_mem = '768MB';
+    ALTER SYSTEM SET max_connections = '20';
+    ALTER SYSTEM SET shared_buffers = '16GB';
+    ALTER SYSTEM SET effective_cache_size = '48GB';
+    ALTER SYSTEM SET maintenance_work_mem = '2GB';
     ALTER SYSTEM SET checkpoint_completion_target = '0.9';
     ALTER SYSTEM SET wal_buffers = '16MB';
-    ALTER SYSTEM SET default_statistics_target = '100';
-    ALTER SYSTEM SET random_page_cost = '4';
-    ALTER SYSTEM SET effective_io_concurrency = '2';
-    ALTER SYSTEM SET work_mem = '15728kB';
-    ALTER SYSTEM SET min_wal_size = '1GB';
-    ALTER SYSTEM SET max_wal_size = '2GB';
-    ALTER SYSTEM SET max_worker_processes = '2';
-    ALTER SYSTEM SET max_parallel_workers_per_gather = '1';
-    ALTER SYSTEM SET max_parallel_workers = '2';
+    ALTER SYSTEM SET default_statistics_target = '500';
+    ALTER SYSTEM SET random_page_cost = '1.1';
+    ALTER SYSTEM SET effective_io_concurrency = '200';
+    ALTER SYSTEM SET work_mem = '209715kB';
+    ALTER SYSTEM SET min_wal_size = '4GB';
+    ALTER SYSTEM SET max_wal_size = '8GB';
+    ALTER SYSTEM SET max_worker_processes = '4';
+    ALTER SYSTEM SET max_parallel_workers_per_gather = '2';
+    ALTER SYSTEM SET max_parallel_workers = '4';
 EOSQL
 }
 
